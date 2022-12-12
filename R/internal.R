331b14713e730af50246579444082dc888e4146d
Stéphane Laurent <laurent_step@yahoo.fr>
2022-12-12 18:00:42 +0100

    isFraction

--- R/internal.R
+++ R/internal.R
@@ -6,6 +6,44 @@ isCoeffs <- function(x) {
   is.bigq(x) && !anyNA(x)
 }
 
+isInteger <- function(x) {
+  is.numeric(x) && length(x) == 1L && !is.na(x) && as.integer(x) == x
+}
+
 isPositiveInteger <- function(x) {
   is.numeric(x) && length(x) == 1L && !is.na(x) && floor(x) == x
+}
+
+isNonnegativeInteger <- function(x) {
+  is.numeric(x) && length(x) == 1L && !is.na(x) && floor(x) == x && x != 0
+}
+
+isFraction <- function(x) {
+  if(!is.character(x) || length(x) != 1L) {
+    return(FALSE)
+  }
+  nd <- strsplit(x, "/")[[1L]]
+  if(nd != 2L) {
+    FALSE
+  } else {
+    n <- nd[1L]
+    if(!grepl("^\\-*\\d+$")) {
+      FALSE
+    } else {
+      d <- nd[2L]
+      if(!grepl("^\\d+$") || grepl("^0+$")) {
+        FALSE
+      } else {
+        TRUE
+      }
+    }
+  }
+}
+
+isExponents <- function(x) {
+  is.numeric(x) && !anyNA(x) && all(floor(x) == x)
+}
+
+isCoeffs <- function(x) {
+  all(vapply(x, isFraction, FUN.VALUE = logical(1L)))
 }
