694fccc0769f79aa50c3b3d951ec93b874250043
stla <laurent_step@yahoo.fr>
2023-07-14 23:33:46 +0200

    essai elimination

--- inst/essais/essai_groebner_elimination01.R
+++ inst/essais/essai_groebner_elimination01.R
@@ -9,15 +9,15 @@ f3 <- x + y + z^2 - 1
 gb <- groebner(list(f1, f2, f3))
 ( gbstrings <- lapply(gb, prettyQspray, vars = c("x", "y", "z")) )
 
-yac_str("Factors(z^6 - z^2 + 4*z^3 - 4*z^4)")
+yac_str("Factor(z^6 - z^2 + 4*z^3 - 4*z^4)")
 yac_str("factor1 := 2*z + z^2 - 1")
 yac_str("roots := PSolve(2*z + z^2 - 1, z)")
 yac_str("root1 := Nth(roots, 1)") # Sqrt(2) - 1
-yac_str("root2 := Nth(roots, 2)")
+yac_str("root2 := Nth(roots, 2)") # -Sqrt(2) - 1
 
 yac_str(sprintf("p3 := %s", gbstrings[[3L]]))
-yac_str("PSolve(Eliminate(z, root1, p3), y)")
-yac_str("N(PSolve(Eliminate(z, root1, p3), y))")
+yac_str("PSolve(Eliminate(z, Sqrt(2)-1, p3), y)")
+yac_str("N(PSolve(Eliminate(z, Sqrt(2)-1, p3), y))")
 
 yac_str(sprintf("p2 := %s", gbstrings[[2L]]))
 yac_str("PSolve(Eliminate(z, Sqrt(2)-1, p2), y)")
