a9f453021a444287028c5da33330b74a938a0b63
stla <laurent_step@yahoo.fr>
2024-04-01 19:00:09 +0200

    skewSchurPol

--- inst/essais/HallInnerProduct01.R
+++ inst/essais/HallInnerProduct01.R
@@ -40,17 +40,42 @@ t + 2 # no
 # 
 library(jack)
 
-SkewJackPol <- function(n, lambda, mu, alpha) {
+#' Skew Jack polynomial
+#' @description Computes a skew Jack polynomial.
+#' 
+#' @param n positive integer, the number of variables
+#' @param lambda outer integer partition of the skew partition
+#' @param mu inner integer partition of the skew partition; it must be a 
+#'   subpartition of \code{lambda}
+#' @param alpha the Jack parameter, an integer or a \code{bigq} number
+#' @param which which Jack polynomial, \code{"J"}, \code{"P"} or \code{"Q"}
+#'
+#' @return A \code{qspray} polynomial.
+#' @export
+#' @importFrom gmp is.bigq as.bigq
+#' @importFrom partitions parts
+#' @importFrom qspray PSPexpression HallInnerProduct
+#' 
+#' @details
+#' Gröbner bases are used in the algorithm, and this is slow.
+#' 
+#'
+#' @examples
+#' SkewJackPol(3, c(3,1), c(2), 2)
+SkewJackPol <- function(n, lambda, mu, alpha, which = "J") {
+  stopifnot(jack:::isPositiveInteger(n))
   stopifnot(jack:::isPartition(lambda), jack:::isPartition(mu))
   mu <- c(mu, rep(0L, length(lambda) - length(mu)))
   if(any(lambda - mu < 0L)) {
     stop("The partition `mu` is not a subpartition of the partition `lambda`.")
   }
-  Jlambda <- PSPexpression(JackPolCPP(n, lambda, alpha))
-  Jmu     <- JackPolCPP(n, mu, alpha)
+  stopifnot(jack:::isInteger(alpha) || is.bigq(alpha))
+  alpha <- as.bigq(alpha)
+  Jlambda <- PSPexpression(JackPolCPP(n, lambda, alpha, which))
+  Jmu     <- JackPolCPP(n, mu, alpha, which)
   nus <- partitions::parts(sum(lambda) - sum(mu))
   terms <- apply(nus, 2L, function(nu) {
-    Jnu <- JackPolCPP(n, nu, alpha)
+    Jnu <- JackPolCPP(n, nu, alpha, which)
     coeff <- HallInnerProduct(Jlambda, Jmu * Jnu, alpha) /
       HallInnerProduct(Jnu, Jnu, alpha) 
     coeff * Jnu
