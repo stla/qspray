a46ef1ad59f4571e645dd54251b18bb4f34ddf34
stla <laurent_step@yahoo.fr>
2024-04-05 05:32:41 +0200

    moved utils to qspray.h

--- inst/include/qspray.h
+++ inst/include/qspray.h
@@ -1,7 +1,7 @@
 #ifndef __HEADER__
 #define __HEADER__
 
-#include <RcppArmadillo.h>
+#include <Rcpp.h>
 #include <boost/multiprecision/gmp.hpp>
 #include <complex.h>
 typedef std::vector<signed int>                             powers;
@@ -9,9 +9,67 @@ typedef boost::multiprecision::mpq_rational                 gmpq;
 typedef boost::multiprecision::mpz_int                      gmpi;
 typedef std::complex<gmpq>                                  qcplx;
 
-std::string q2str(gmpq);
-void simplifyPowers(powers&);
-powers growPowers(powers, signed int, signed int);
+
+// -------------------------------------------------------------------------- //
+std::string q2str(gmpq r) {
+  const gmpi numer = boost::multiprecision::numerator(r);
+  const gmpi denom = boost::multiprecision::denominator(r);
+  mpz_t p;
+  mpz_init(p);
+  mpz_set(p, numer.backend().data());
+  mpz_t q;
+  mpz_init(q);
+  mpz_set(q, denom.backend().data());
+  const size_t n = mpz_sizeinbase(p, 10) + 2;
+  const size_t d = mpz_sizeinbase(q, 10) + 2;
+  char* cnumer = new char[n];
+  char* cdenom = new char[d];
+  cnumer = mpz_get_str(cnumer, 10, p);
+  cdenom = mpz_get_str(cdenom, 10, q);
+  std::string snumer = cnumer;
+  std::string sdenom = cdenom;
+  delete[] cnumer;
+  delete[] cdenom;
+  mpz_clear(p);
+  mpz_clear(q);
+  return snumer + "/" + sdenom;
+}
+
+
+// -------------------------------------------------------------------------- //
+void simplifyPowers(powers& pows) {
+  int n = pows.size();
+  if(n == 0) {
+    return;
+  }
+  n--;
+  powers::iterator it = pows.end();
+  bool zero = pows[n] == 0;
+  while(zero && n > 0) {
+    it--;
+    n--;
+    zero = pows[n] == 0;
+  }
+  if(zero) {
+    pows = {};
+  } else {
+    pows.erase(it, pows.end());
+  }
+}
+
+// -------------------------------------------------------------------------- //
+powers growPowers(powers pows, signed int m, signed int n) {
+  powers gpows;
+  gpows.reserve(n);
+  for(signed int i = 0; i < m; i++) {
+    gpows.emplace_back(pows[i]);
+  }
+  for(signed int i = m; i < n; i++) {
+    gpows.emplace_back(0);
+  }
+  return gpows;
+}
+
 
 // -------------------------------------------------------------------------- //
 namespace QSPRAY {
