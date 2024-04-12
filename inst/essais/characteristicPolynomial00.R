2783b07f04f081fd6221d665c85ce6897766fdba
stla <laurent_step@yahoo.fr>
2024-04-12 19:26:00 +0200

    characteristic polynomial

--- inst/essais/characteristicPolynomial00.R
+++ inst/essais/characteristicPolynomial00.R
@@ -3,11 +3,12 @@ library(qspray)
 makeTheMatrix <- function(A) {
   d <- nrow(A)
   M <- list()
+  lambda <- qlone(1L)
   for(i in seq_len(d)) {
     for(j in seq_len(d)) {
       indices <- toString(c(i, j))
       if(i == j) {
-        entry <- A[i, j][] - qlone(1)
+        entry <- A[i, j][] - lambda
       } else {
         entry <- A[i, j][]
       }
@@ -24,8 +25,8 @@ dropRow <- function(M, i) {
   ncol <- attr(M, "ncol")
   Mnew <- list()
   for(k in seq_len(nrow-1L)) {
+    ii <- if(k < i) k else k+1L
     for(j in seq_len(ncol)) {
-      ii <- if(k < i) k else k+1L
       Mnew[[toString(c(k, j))]] <- M[[toString(c(ii, j))]]
     }
   }
@@ -38,9 +39,9 @@ dropCol <- function(M, j) {
   nrow <- attr(M, "nrow")
   ncol <- attr(M, "ncol")
   Mnew <- list()
-  for(i in seq_len(nrow)) {
-    for(k in seq_len(ncol - 1L)) {
-      jj <- if(k < j) k else k+1L
+  for(k in seq_len(ncol-1L)) {
+    jj <- if(k < j) k else k+1L
+    for(i in seq_len(nrow)) {
       Mnew[[toString(c(i, k))]] <- M[[toString(c(i, jj))]]
     }
   }
@@ -58,7 +59,7 @@ detLaplace <- function(M) {
   if(d == 1L) {
     M[[toString(c(1L, 1L))]]
   } else {
-    result <- 0L
+    result <- qzero()
     for(i in seq_len(d)) {
       entry <- M[[toString(c(i, 1L))]]
       if(i %% 2L == 0L) {
@@ -71,10 +72,22 @@ detLaplace <- function(M) {
   }
 }
 
-#' @importFrom gmp is.matrixZQ
+#' @title Characteristic polynomial
+#' @description Characteristic polynomial of a matrix.
+#' 
+#' @param A a square matrix with numeric, character, or \code{bigq} entries
+#' 
+#' @return A univariate \code{qspray} polynomial.
+#' @export
+#' @importFrom gmp is.matrixZQ as.bigq
+#' 
+#' @examples
+#' set.seed(666)
+#' A <- matrix(rpois(9L, 10), nrow = 3L, ncol = 3L)
+#' characteristicPolynomial(A)
 characteristicPolynomial <- function(A) {
   if(!is.matrix(A) && !is.matrixZQ(A)) {
-    stop("A must be a matrix.")
+    stop("`A` must be a matrix.")
   }
   stopifnot(nrow(A) == ncol(A))
   A <- as.bigq(A)
@@ -84,8 +97,9 @@ characteristicPolynomial <- function(A) {
   detLaplace(makeTheMatrix(A))
 }
 
+set.seed(666)
 A <- matrix(rpois(9L, 10), nrow = 3L, ncol = 3L)
-p <- characteristicPolynomial(A)
+characteristicPolynomial(A)
 
 
 
