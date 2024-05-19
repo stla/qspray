f1fa00425ed1805553b2cf6ae7db9c89c8acaabf
stla <laurent_step@yahoo.fr>
2024-05-19 22:46:42 +0200

    invTriMatrix

---
+++ inst/essais/essai_aLambdaMu.R
@@ -0,0 +1,68 @@
+library(qspray)
+library(syt)
+library(partitions)
+
+aLambdaMu <- function(lambda, mu) {
+  partitions <- parts(sum(lambda))
+  KN1 <- apply(partitions, 2L, function(kappa) {
+    KostkaNumber(kappa, lambda)
+  })
+  KN2 <- apply(partitions, 2L, function(kappa) {
+    KostkaNumber(kappa, mu)
+  })
+  sum(KN1 * KN2)
+}
+
+bLambdaMu <- function(lambda, mu) {
+  partitions <- parts(sum(lambda))
+  KN1 <- apply(partitions, 2L, function(kappa) {
+    KostkaNumber(kappa, lambda)
+  })
+  KN2 <- apply(partitions, 2L, function(kappa) {
+    KostkaNumber(conjugate(kappa), mu)
+  })
+  sum(KN1 * KN2)
+}
+
+aLambdaMu(c(4, 1, 1), c(3, 3))
+HallInnerProduct(CSHFpoly(6, c(4, 1, 1)), CSHFpoly(6, c(3, 3)))
+
+bLambdaMu(c(1, 1, 1, 1, 1, 1), c(3, 3))
+HallInnerProduct(CSHFpoly(6, c(1, 1, 1, 1, 1, 1)), ESFpoly(6, c(3,3)))
+
+
+#          for j=2:m
+#              for i=1:j-1
+#                  Ci(i,j) = -Ci(i,i:j-1)*Ci(i:j-1,j);
+#              end
+#          end
+km <- jack::KostkaNumbers(4, "2")
+library(gmp)
+Ci <- as.bigq(km)
+for(j in 2:5) {
+  for(i in 1:(j-1)) {
+    Ci[i, j] <- -sum(c(Ci[i, i:(j-1)]) * c(Ci[i:(j-1), j]))
+  }
+}
+Ci
+RationalMatrix::Qinverse(km)
+
+invTriMatrix <- function(A) {
+  d <- nrow(A)
+  if(d == 1L) {
+    return(A)
+  } else {
+    B <- invTriMatrix(A[1L:(d-1L), 1L:(d-1L)])
+    newColumn <- as.bigq(integer(d-1L))
+    for(i in 1L:(d-1L)) {
+      newColumn[i] <- -sum(c(B[i, i:(d-1L)]) * c(A[i:(d-1L), d]))
+    }
+    newRow <- integer(d)
+    newRow[d] <- 1L
+    B <- rbind(cbind(B, newColumn), as.bigq(newRow))
+    return(B)
+  }
+}
+km <- jack::KostkaNumbers(4, "2")
+invTriMatrix(as.bigq(km))
+RationalMatrix::Qinverse(km)
